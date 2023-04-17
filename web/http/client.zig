9987bcd80fed0c3680595a7a74f3aa2fecf6b853
Matheus C. França <matheus-catarino@hotmail.com>
2023-04-17 16:31:59 -0300

    More changes:
    * http server added
    * update http/tls client (kyber)
    * refactoring build-system
    * CI - Windows
    * Update .github/workflows/CI.yml
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- web/http/client.zig
+++ web/http/client.zig
@@ -1,12 +1,17 @@
 const std = @import("std");
 
 test "Status == 200" {
-    const uri = std.Uri.parse("https://ziglang.org/") catch unreachable;
+    const uri = std.Uri.parse("https://ziglang.org") catch unreachable;
     var client: std.http.Client = .{ .allocator = std.testing.allocator };
     defer client.deinit();
+    var h = std.http.Headers{ .allocator = std.testing.allocator };
+    defer h.deinit();
 
-    var req = try client.request(uri, .{}, .{});
+    var req = try client.request(.GET, uri, h, .{});
     defer req.deinit();
-
-    try std.testing.expect(req.response.headers.status == .ok);
+    try req.start(); // start request
+    try req.finish(); // end request
+    try req.wait(); //wait response
+    try std.testing.expect(req.response.status == .ok);
+    try std.testing.expect(req.response.version == .@"HTTP/1.1");
 }
--- web/tls/X25519+Kyber768Draft00.zig
+++ web/tls/X25519+Kyber768Draft00.zig
@@ -2,23 +2,27 @@
 //! by: https://github.com/bwesterb
 
 test "HTTPS Client - X25519+Kyber768Draft00" {
-    const hdrs = std.http.Client.Request.Headers{};
-    const opts = std.http.Client.Request.Options{};
     var buf: [1000]u8 = undefined;
+    var header_buf: [8192]u8 = undefined;
     const uri = try std.Uri.parse("https://cloudflare.com/cdn-cgi/trace");
     var client = std.http.Client{
         .allocator = testing.allocator,
     };
     defer client.deinit();
+    var h = std.http.Headers{ .allocator = testing.allocator };
+    defer h.deinit();
 
-    var req = try client.request(uri, hdrs, opts);
-    try req.finish();
+    var req = try client.request(.POST, uri, h, .{
+        .header_strategy = .{ .static = &header_buf },
+    });
     defer req.deinit();
-    const read = try req.readAll(&buf);
+    try req.start();
+    try req.finish();
+    try req.wait();
+    const read = try req.read(&buf);
 
     var strings = std.mem.split(u8, buf[0..read], "\n");
     var index = strings.index.?;
-
     while (index < strings.rest().len) : (index += 1) {
         const content = strings.next().?;
         if (startW(u8, content, "h="))
@@ -27,8 +31,9 @@ test "HTTPS Client - X25519+Kyber768Draft00" {
             try testing.expectEqualStrings("visit_scheme=https", content);
         if (startW(u8, content, "http="))
             try testing.expectEqualStrings("http=http/1.1", content);
-        if (startW(u8, content, "uag="))
-            try testing.expectEqualStrings("uag=zig (std.http)", content);
+        // if (startW(u8, content, "uag="))
+        //     try testing.expectEqualStrings("uag=zig (std.http)", content);
+        // zig master/nightly change per build (e.g.: zig/0.11.0-dev.2868+1a455b2dd (std.http))
         if (startW(u8, content, "tls="))
             try testing.expectEqualStrings("tls=TLSv1.3", content);
     }
