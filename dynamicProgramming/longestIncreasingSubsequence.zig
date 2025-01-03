47f1ce5d55813e5dd544ba1e51f477a77bcfc697
Spiros Maggioros <spirosastro@gmail.com>
2025-01-03 20:33:45 +0200

    Added allocator support in function

--- dynamicProgramming/longestIncreasingSubsequence.zig
+++ dynamicProgramming/longestIncreasingSubsequence.zig
@@ -26,12 +26,10 @@ pub fn lower_bound(arr: []const i32, key: i32) usize {
 
 // Function that returns the length of the longest increasing subsequence of an array
 // Runs in O(nlogn) using the lower bound function
-pub fn lis(arr: []const i32) usize {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-
-    const allocator = gpa.allocator();
-
+// Arguments:
+//      arr: the passed array
+//      allocator: Any std.heap type allocator
+pub fn lis(arr: []const i32, allocator: anytype) usize {
     var v = ArrayList(i32).init(allocator);
     defer v.deinit();
 
@@ -50,18 +48,21 @@ pub fn lis(arr: []const i32) usize {
 }
 
 test "testing longest increasing subsequence function" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
     const v = [4]i32{ 1, 5, 6, 7 };
-    try testing.expect(lis(&v) == 4);
+    try testing.expect(lis(&v, gpa.allocator()) == 4);
 
     const v2 = [5]i32{ 1, -1, 5, 6, 7 };
-    try testing.expect(lis(&v2) == 4);
+    try testing.expect(lis(&v2, gpa.allocator()) == 4);
 
     const v3 = [5]i32{ 1, 2, -1, 0, 1 };
-    try testing.expect(lis(&v3) == 3);
+    try testing.expect(lis(&v3, gpa.allocator()) == 3);
 
     const v4 = [0]i32{};
-    try testing.expect(lis(&v4) == 0);
+    try testing.expect(lis(&v4, gpa.allocator()) == 0);
 
     const v5 = [5]i32{ 0, 0, 0, 0, 0 };
-    try testing.expect(lis(&v5) == 1);
+    try testing.expect(lis(&v5, gpa.allocator()) == 1);
 }
